f93ec78cbe7015cb2af5bd0d97c122ec1f587f49
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-02 14:16:31 +0100

    Create Product.java
    -Created base class for all the products in the store

---
+++ src/main/java/com/mateusz113/shop/model/Product.java
@@ -0,0 +1,37 @@
+package com.mateusz113.shop.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private final String id;
+    private final String name;
+    private BigDecimal price;
+    private int quantity;
+
+    public Product(String id, String name, BigDecimal price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
