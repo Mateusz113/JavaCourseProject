695e839b7ed413bfbc1a908070b0da2a02aefa96
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-02 22:25:17 +0100

    Create ShopApp.java
    -Base application class with main method

---
+++ src/main/java/com/mateusz113/shop/app/ShopApp.java
@@ -0,0 +1,8 @@
+package com.mateusz113.shop.app;
+
+public class ShopApp {
+    public static void main(String[] args) {
+        ShopControl shopControl = new ShopControl();
+        shopControl.start();
+    }
+}
