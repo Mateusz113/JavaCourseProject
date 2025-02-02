e21f1620b2462dbaf810e7b03761555b187b1e9c
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-02 14:17:55 +0100

    Create Phone.java
    -Subclass of Product that will be used to hold the specified information about phones sold in the store

---
+++ src/main/java/com/mateusz113/shop/model/phone/Phone.java
@@ -0,0 +1,44 @@
+package com.mateusz113.shop.model.phone;
+
+import com.mateusz113.shop.model.Product;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Phone extends Product {
+    private String phoneColor;
+    private int batterySize;
+    private List<PhoneAccessory> accessories;
+
+    public Phone(String id, String name, BigDecimal price, int quantity) {
+        super(id, name, price, quantity);
+        this.phoneColor = "Czarny";
+        this.batterySize = 4000;
+        this.accessories = new ArrayList<>();
+    }
+
+    public String getPhoneColor() {
+        return phoneColor;
+    }
+
+    public void setPhoneColor(String phoneColor) {
+        this.phoneColor = phoneColor;
+    }
+
+    public int getBatterySize() {
+        return batterySize;
+    }
+
+    public void setBatterySize(int batterySize) {
+        this.batterySize = batterySize;
+    }
+
+    public List<PhoneAccessory> getAccessories() {
+        return accessories;
+    }
+
+    public void addAccessory(PhoneAccessory pa) {
+        accessories.add(pa);
+    }
+}
