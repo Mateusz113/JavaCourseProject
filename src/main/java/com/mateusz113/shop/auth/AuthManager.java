f832b040a24206deb46f8c85349ba8fdcf461f35
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-03 17:23:23 +0100

    Update AuthManager.java
    -Updated the classes based on the file handlers changes

--- src/main/java/com/mateusz113/shop/auth/AuthManager.java
+++ src/main/java/com/mateusz113/shop/auth/AuthManager.java
@@ -2,8 +2,8 @@ package com.mateusz113.shop.auth;
 
 import com.mateusz113.shop.exception.NoSuchUserException;
 import com.mateusz113.shop.exception.UserAlreadyExistsException;
-import com.mateusz113.shop.io.file.ShopFileReader;
-import com.mateusz113.shop.io.file.ShopFileWriter;
+import com.mateusz113.shop.io.file.user.UserFileReader;
+import com.mateusz113.shop.io.file.user.UserFileWriter;
 import com.mateusz113.shop.model.User;
 
 import java.io.IOException;
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 public class AuthManager {
-    public User registerUser(RegisterDetails registerDetails, ShopFileWriter writer, ShopFileReader reader) throws UserAlreadyExistsException, IOException {
+    public User registerUser(RegisterDetails registerDetails, UserFileWriter writer, UserFileReader reader) throws UserAlreadyExistsException, IOException {
         List<String> usedEmails = reader.getUsersLoginDetails().stream().map(LoginDetails::email).toList();
         boolean emailInUse = usedEmails.stream().anyMatch(usedEmail -> usedEmail.equals(registerDetails.email()));
         if (emailInUse) throw new UserAlreadyExistsException("Użytkownik z takim adresem email już istnieje!");
@@ -20,12 +20,12 @@ public class AuthManager {
         return new User(id, registerDetails.firstName(), registerDetails.lastName(), registerDetails.email());
     }
 
-    public User loginUser(LoginDetails loginDetails, ShopFileReader reader) throws IOException, NoSuchUserException {
+    public User loginUser(LoginDetails loginDetails, UserFileReader reader) throws IOException, NoSuchUserException {
         String userId = getIdFromLoginDetails(loginDetails, reader);
         return getUserFromId(userId, reader);
     }
 
-    private String getIdFromLoginDetails(LoginDetails loginDetails, ShopFileReader reader) throws IOException, NoSuchUserException {
+    private String getIdFromLoginDetails(LoginDetails loginDetails, UserFileReader reader) throws IOException, NoSuchUserException {
         String loginInfo = reader.getLoginInfoFromLoginDetails(loginDetails);
         try {
             return loginInfo.split(";")[2];
@@ -34,7 +34,7 @@ public class AuthManager {
         }
     }
 
-    private User getUserFromId(String id, ShopFileReader reader) throws IOException, NoSuchUserException {
+    private User getUserFromId(String id, UserFileReader reader) throws IOException, NoSuchUserException {
         String compactedDetails = reader.getUserInfoFromId(id);
         String[] details = compactedDetails.split(";");
         try {
