a3d266419516c1fdd16a5299f2dafb8117bfd853
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-03 17:23:44 +0100

    Update ConsolePrinter.java
    -Added a method to print all the products to the console

--- src/main/java/com/mateusz113/shop/io/ConsolePrinter.java
+++ src/main/java/com/mateusz113/shop/io/ConsolePrinter.java
@@ -1,6 +1,8 @@
 package com.mateusz113.shop.io;
 
-import com.mateusz113.shop.app.menu_navigation.AuthOption;
+import com.mateusz113.shop.model.Product;
+
+import java.util.List;
 
 public class ConsolePrinter {
     public static void printLine(String line) {
@@ -9,6 +11,15 @@ public class ConsolePrinter {
 
     public static void printGreeting() {
         printLine("***** WITAMY W APLIKACJI ELECTRONICS SHOP *****");
+    }
+
+    public static void printProducts(List<Product> products) {
+        int i = 1;
+        for (Product p : products) {
+            printLine(i + ".");
+            printLine(p.toString());
+            i++;
+        }
     }
 
     public static <T> void printMenuOptions(T[] options) {
