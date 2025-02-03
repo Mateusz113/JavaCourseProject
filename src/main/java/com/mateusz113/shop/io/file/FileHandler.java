c5344b1d7f7748ad0fc5bdcf86be44492cce23b4
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-03 13:04:41 +0100

    Update FileHandler.java
    -Added inforamtion about the path to the product details files

--- src/main/java/com/mateusz113/shop/io/file/FileHandler.java
+++ src/main/java/com/mateusz113/shop/io/file/FileHandler.java
@@ -3,10 +3,12 @@ package com.mateusz113.shop.io.file;
 public abstract class FileHandler {
     private final String userLoginInfoPath;
     private final String userDetailsPath;
+    private final String productDetailsPath;
 
-    public FileHandler(String userLoginInfoPath, String userDetailsPath) {
+    public FileHandler(String userLoginInfoPath, String userDetailsPath, String productDetailsPath) {
         this.userLoginInfoPath = userLoginInfoPath;
         this.userDetailsPath = userDetailsPath;
+        this.productDetailsPath = productDetailsPath;
     }
 
     public String getUserLoginInfoPath() {
@@ -15,5 +17,9 @@ public abstract class FileHandler {
 
     public String getUserDetailsPath() {
         return userDetailsPath;
+    }
+
+    public String getProductDetailsPath() {
+        return productDetailsPath;
     }
 }
