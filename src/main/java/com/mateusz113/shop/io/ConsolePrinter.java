359733375ca706c6a4ffa8e76af13eca567b6c3f
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-04 16:21:20 +0100

    Update ConsolePrinter.java
    -Added functions to print previous orders and cart

--- src/main/java/com/mateusz113/shop/io/ConsolePrinter.java
+++ src/main/java/com/mateusz113/shop/io/ConsolePrinter.java
@@ -1,7 +1,11 @@
 package com.mateusz113.shop.io;
 
+import com.mateusz113.shop.model.Cart;
+import com.mateusz113.shop.model.Order;
 import com.mateusz113.shop.model.Product;
 
+import java.math.BigDecimal;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class ConsolePrinter {
@@ -13,7 +17,7 @@ public class ConsolePrinter {
         printLine("***** WITAMY W APLIKACJI ELECTRONICS SHOP *****");
     }
 
-    public static void printProductsManagementMenu(){
+    public static void printProductsManagementMenu() {
         printLine("Co chcesz zrobić?");
         printLine("0. Wróć do głównego menu");
         printLine("1. Dodaj produkt");
@@ -23,9 +27,21 @@ public class ConsolePrinter {
     public static void printProducts(List<Product> products) {
         int i = 1;
         for (Product p : products) {
-            printLine(i + ".");
-            printLine(p.toString());
-            i++;
+            if (p.getQuantity() > 0) {
+                printLine(i + ".");
+                printLine(p.toString());
+                i++;
+            }
+        }
+    }
+
+    public static void printPreviousOrders(List<Order> orders) {
+        printLine("Twoje poprzednie zamówienia:");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        for (Order order : orders) {
+            String formattedDate = order.placementTime().format(formatter);
+            printLine("Data: " + formattedDate);
+            printProducts(order.products());
         }
     }
 
@@ -33,5 +49,16 @@ public class ConsolePrinter {
         for (T option : options) {
             printLine(option.toString());
         }
+    }
+
+    public static void printCart(Cart cart) {
+        printLine("Wózek:\n");
+        printProducts(cart.getProducts());
+        BigDecimal sumOfPrices = cart
+                .getProducts()
+                .stream()
+                .map(product -> BigDecimal.valueOf(product.getPrice().doubleValue() * product.getQuantity()))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        printLine(String.format("Całkowita wartość twojego koszyka wynosi: %.2f zł", sumOfPrices));
     }
 }
