e2e40efd552bd2fccf9be75fb29f28d8618fa389
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-06 18:00:29 +0100

    Update OrderManager.java
    -Added a function to output invoice to desktop based on order and user

--- src/main/java/com/mateusz113/shop/manager/OrderManager.java
+++ src/main/java/com/mateusz113/shop/manager/OrderManager.java
@@ -2,11 +2,14 @@ package com.mateusz113.shop.manager;
 
 import com.mateusz113.shop.model.Order;
 import com.mateusz113.shop.model.Product;
+import com.mateusz113.shop.model.User;
 
-import java.io.Serializable;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.mateusz113.shop.util.ProductUtil.getTotalProductsPrice;
 
 public class OrderManager implements Serializable {
     private final List<Order> orders = new ArrayList<>();
@@ -17,5 +20,32 @@ public class OrderManager implements Serializable {
 
     public void addNewOrder(String userId, List<Product> products) {
         orders.add(new Order(userId, LocalDateTime.now(), products));
+    }
+
+    public void outputInvoice(Order order, User user) throws IOException {
+        String desktopPath = System.getProperty("user.home") + File.separator + "Desktop";
+        LocalDateTime orderPlacementTime = order.placementTime();
+        String fileName = String.format("Faktura z dnia %d.%d - godz. %d_%d.txt",
+                orderPlacementTime.getDayOfMonth(),
+                orderPlacementTime.getMonthValue(),
+                orderPlacementTime.getHour(),
+                orderPlacementTime.getMinute()
+        );
+        String filePath = desktopPath + File.separator + fileName;
+        try (
+                BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))
+        ) {
+            writer.write("****** FAKTURA ZE SKLEPU \"ELECTRONICS SHOP\" ******\n");
+            writer.write("Wystawiający: Electronics Shop\n");
+            writer.write(String.format("Kupujący: %s %s%n", user.firstName(), user.lastName()));
+            writer.write(String.format("Email kupującego: %s%n", user.email()));
+            writer.write("Zamówione produkty:\n");
+            for (Product p : order.products()) {
+                writer.write(p.toString());
+                writer.newLine();
+            }
+            writer.write(String.format("Całkowita kwota zamówienia wyniosła: %.2f zł%n", getTotalProductsPrice(order.products())));
+            writer.write("Podpisano: Electronics Shop");
+        }
     }
 }
