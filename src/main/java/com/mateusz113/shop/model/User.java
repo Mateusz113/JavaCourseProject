a773499a0d1ba5971ebe52f31ac402579644e0c5
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-07 12:13:56 +0100

    Update User.java

--- src/main/java/com/mateusz113/shop/model/User.java
+++ src/main/java/com/mateusz113/shop/model/User.java
@@ -2,6 +2,14 @@ package com.mateusz113.shop.model;
 
 import java.io.Serializable;
 
+/**
+ * Class that holds the information about the user.
+ *
+ * @param id ID of the user.
+ * @param firstName first name of the user.
+ * @param lastName last name of the user.
+ * @param email email of the user.
+ */
 public record User(
         String id,
         String firstName,
