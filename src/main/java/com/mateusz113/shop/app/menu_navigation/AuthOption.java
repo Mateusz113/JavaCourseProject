fbcb96693e5a7b43747fbe84e8b9f70ddd2d38f6
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-07 01:03:26 +0100

    Update AuthOption.java
    -Added documentation

--- src/main/java/com/mateusz113/shop/app/menu_navigation/AuthOption.java
+++ src/main/java/com/mateusz113/shop/app/menu_navigation/AuthOption.java
@@ -2,11 +2,20 @@ package com.mateusz113.shop.app.menu_navigation;
 
 import com.mateusz113.shop.exception.NoSuchOptionException;
 
+/**
+ * Class representing options in app authentication loop.
+ */
 public enum AuthOption {
     EXIT(0, "Opuść program"), REGISTER(1, "Rejestracja"), LOGIN(2, "Login");
     private int value;
     private String label;
 
+    /**
+     * Constructor for the authentication option.
+     *
+     * @param value numerical value of the option.
+     * @param label label describing the option to the user.
+     */
     AuthOption(int value, String label) {
         this.value = value;
         this.label = label;
@@ -17,6 +26,13 @@ public enum AuthOption {
         return String.format("%d. %s", value, label);
     }
 
+    /**
+     * Retrieves {@code AuthOption} from the numerical value.
+     *
+     * @param value numerical value of the authentication option.
+     * @return the corresponding {@code AuthOption}
+     * @throws NoSuchOptionException if the provided value has no associated option.
+     */
     public static AuthOption getOptionFromValue(int value) throws NoSuchOptionException {
         try {
             return AuthOption.values()[value];
