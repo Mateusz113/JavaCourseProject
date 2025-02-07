2fbb00a5b7c7f630e115d72504ed17eb2231e2c7
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-07 12:13:59 +0100

    Update ProductUtil.java

--- src/main/java/com/mateusz113/shop/util/ProductUtil.java
+++ src/main/java/com/mateusz113/shop/util/ProductUtil.java
@@ -5,7 +5,16 @@ import com.mateusz113.shop.model.Product;
 import java.math.BigDecimal;
 import java.util.List;
 
+/**
+ * Class holding the utility functions for the product.
+ */
 public class ProductUtil {
+    /**
+     * Retrieve the total cost of a product list.
+     *
+     * @param products list of products that price will be calculated of.
+     * @return {@code BigDecimal} representing the total list price.
+     */
     public static BigDecimal getTotalProductsPrice(List<Product> products) {
         return products
                 .stream()
