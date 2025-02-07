dd88e97b09f86555c5d3109f4d899e7edefc0374
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-07 01:03:34 +0100

    Update LoginDetails.java
    -Added documentation

--- src/main/java/com/mateusz113/shop/auth/LoginDetails.java
+++ src/main/java/com/mateusz113/shop/auth/LoginDetails.java
@@ -2,7 +2,16 @@ package com.mateusz113.shop.auth;
 
 import java.io.Serializable;
 
+/**
+ * Class representing user login details collected from console.
+ *
+ * @param email user email.
+ * @param password user password.
+ */
 public record LoginDetails(String email, String password) implements Serializable {
+    /**
+     * Builder class for {@code LoginDetails}.
+     */
     public static class LoginDetailsBuilder {
         private String email;
         private String password;
@@ -20,6 +29,12 @@ public record LoginDetails(String email, String password) implements Serializabl
             return this;
         }
 
+        /**
+         * Builder build method.
+         *
+         * @return {@code LoginDetails} instance with builder fields.
+         * @throws IllegalStateException if not all fields were initialized before calling the build.
+         */
         public LoginDetails build() {
             if (email == null || password == null) {
                 throw new IllegalStateException("Wszystkie zmienne muszą być zainicjowane przed budowaniem!");
@@ -28,6 +43,11 @@ public record LoginDetails(String email, String password) implements Serializabl
         }
     }
 
+    /**
+     * Builder creator method.
+     *
+     * @return builder for the {@code LoginDetails}.
+     */
     public static LoginDetailsBuilder builder() {
         return new LoginDetailsBuilder();
     }
