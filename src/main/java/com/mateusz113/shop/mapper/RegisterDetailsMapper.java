305739a28f023bef97b11f038c8183ffa5af37a2
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-07 12:13:32 +0100

    Update RegisterDetailsMapper.java
    -Added documentation

--- src/main/java/com/mateusz113/shop/mapper/RegisterDetailsMapper.java
+++ src/main/java/com/mateusz113/shop/mapper/RegisterDetailsMapper.java
@@ -4,6 +4,12 @@ import com.mateusz113.shop.auth.LoginDetails;
 import com.mateusz113.shop.auth.RegisterDetails;
 
 public class RegisterDetailsMapper {
+    /**
+     * Maps {@code RegisterDetails} to {@code LoginDetails}.
+     *
+     * @param registerDetails {@code RegisterDetails} to be mapped.
+     * @return {@code LoginDetails} object from provided info.
+     */
     public static LoginDetails toLoginDetails(RegisterDetails registerDetails) {
         return (registerDetails == null) ? null : new LoginDetails(registerDetails.email(), registerDetails.password());
     }
