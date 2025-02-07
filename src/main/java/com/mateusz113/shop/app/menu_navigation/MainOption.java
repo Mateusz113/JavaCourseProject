ed39fa1ad1a1032ba231ee18b4fabebef63e2fb8
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-07 01:03:30 +0100

    Update MainOption.java
    -Added documentation

--- src/main/java/com/mateusz113/shop/app/menu_navigation/MainOption.java
+++ src/main/java/com/mateusz113/shop/app/menu_navigation/MainOption.java
@@ -2,16 +2,24 @@ package com.mateusz113.shop.app.menu_navigation;
 
 import com.mateusz113.shop.exception.NoSuchOptionException;
 
+/**
+ * Class representing options in app main loop.
+ */
 public enum MainOption {
     EXIT(0, "Opuść program"),
     SHOP(1, "Kup produkt"),
     MANAGE_PRODUCTS(2, "Zarządzaj produktami"),
     CART(3, "Koszyk"),
     PREVIOUS_ORDERS(4, "Poprzednie zamówienia");
-
     private int value;
     private String label;
 
+    /**
+     * Constructor for the main option.
+     *
+     * @param value numerical value associated with the option.
+     * @param label label describing option to the user.
+     */
     MainOption(int value, String label) {
         this.value = value;
         this.label = label;
@@ -22,6 +30,13 @@ public enum MainOption {
         return String.format("%d. %s", value, label);
     }
 
+    /**
+     * Retrieves {@code MainOption} based on the numerical value.
+     *
+     * @param value numerical value associated with the option.
+     * @return the corresponding {@code MainOption}.
+     * @throws NoSuchOptionException if provided value has no associated option.
+     */
     public static MainOption getOptionFromValue(int value) throws NoSuchOptionException {
         try {
             return MainOption.values()[value];
